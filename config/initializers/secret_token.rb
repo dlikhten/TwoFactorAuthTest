5874f8e3083352c463d45f8b7e621b3baa3ef0d6
Dmitriy Likhten <dlikhten@gmail.com>
2014-01-28 11:25:51 -0500

    initial prototype with a user model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwoFactorAuthTest::Application.config.secret_key_base = '8fbba95652ef8c6d1ec2212cb3c5ee77ad6a296c46cb70d16fe5549fcb3c7c9859db8ddf079f6740eb3b035d1774c4a97a83402cca4e003676abe2daba653e1f'
